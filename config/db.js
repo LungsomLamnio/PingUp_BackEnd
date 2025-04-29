1bd7c80b8d9a610a346b0cd2dd43d6ee8c95ff4c
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-04-29 22:44:22 +0530

    changes made

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB Connected Successfully");
   } catch (err) {
     console.log(`Database Connection Failed: ${err}`);
