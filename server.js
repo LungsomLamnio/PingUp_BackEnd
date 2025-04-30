f4e1761b4f11229389d0c89da42fbfa3518b4f39
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-04-30 19:50:45 +0530

    cors added

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
 
 require("dotenv").config();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 app.use("/users", authRoutes);
 
 const connectDB = require("./config/db");
