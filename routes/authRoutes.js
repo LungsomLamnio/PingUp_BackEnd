94abe748db9dd8338818ebb6f7572bc39a2b6cf9
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-04-30 17:42:54 +0530

    signup backend

---
+++ routes/authRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 const PORT = 3000;
 
 require("dotenv").config();
+
+app.use("/users", authRoutes);
 
 const connectDB = require("./config/db");
 connectDB();
