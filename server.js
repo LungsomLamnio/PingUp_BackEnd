3351b65cb589f35c4ca4fc87f1d386fdcfba105d
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-05-03 14:58:10 +0530

    cors origin change

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://pingup-frontend-weld.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
